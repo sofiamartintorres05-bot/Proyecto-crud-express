0d8e269d838af8e2b80218998cc9b0b0e67515f1
Sofia <sofiamiraculous@email.com>
2026-08-24 11:20:31 -0500

    Segundo commit (commonis) levantamiento del servidor Tradicional

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const MIPUERTO = process.env.MIPUERTO || 3003;
+
+app.get("/", (req, res) => {
+    res.send("API Rest Full con express");
+});
+
+app.listen(MIPUERTO, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${MIPUERTO}`);
+});
