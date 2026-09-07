583e15797fd5a5ead2eb7f12073e257856376120
Sofia <sofiamiraculous@email.com>
2026-09-07 10:26:25 -0500

    Estructura parse,lectura de archivos json, instalacion de librerias para crear y editar archivos

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
-const express = require("express");
+const { error } = require('console');
+const express = require('express');
 const app = express();
-const MIPUERTO = process.env.MIPUERTO || 3003;
-//middlevare body-paise
+const port = process.env.MIPUERTO || 3003; 
+//libreria fs.path
+const sistemaArchivo = require("fs")
+const ruta = require("path")
+const rutaMiArchivo = ruta.join(__dirname,"datos.json")
+//middlewarc body-parse
 app.use(express.json())
+//middlewarc formateo body
+app.use(express.json())
+app.use(express.urlencoded({extended :true}))
 
 
-app.get("/", (req, res) => {
-    res.send("API Rest Full con express");
+app.get('/', (req, res) => {
+  res.send('API Rest Full con express');
+});
+
+app.get('/api/aprendices', (req, res) => {
+  sistemaArchivo.readFile(rutaMiArchivo, "utf-8", (error, Datos)=>{
+    if (error) res.status(500).json({error : "No se puede leer el archivo"})
+    const listaAprendices = JSON.parse (Datos)
+    res.status(200).json({ Listado : listaAprendices})
+  })
 });
 
 
-app.get("/api/aprendices", (req, res) => {
-    res.status(200).json({'Mensaje': 'Lista Aprendices'})
-})
-
-app.post("/api/aprendices", (req, res) => {
-    const datosAprendiz=req.body
-    const edad =req.body.edad
-    if (edad >= 18) {
-        return res.status(200).json({ Mensaje: 'Eres mayor de edad','Datos':datosAprendiz });
-    }
-    
-    // Si no es mayor de edad, continúa con la creación
-    return res.status(201).json({ Mensaje: 'Crear Aprendiz', datos: datosAprendiz });
+app.post('/api/aprendices', (req, res) => {
+  const datosAprendiz = req.body
+ sistemaArchivo.readFile(rutaMiArchivo, "utf-8", (error, Datos)=>{
+    if (error) res.status(500).json({error : "No se puede leer el archivo"})
+    const listaAprendices = JSON.parse (Datos)
+  listaAprendices.push(datosAprendiz)
+  sistemaArchivo.writeFile(rutaMiArchivo, JSON.stringify(listaAprendices, null, 2), (error)=>{
+    if (error) res.status(500).json({error : "No se puede escribir en el archivo"})
+      res.status(200).json({Mensaje : "Creado", Datos: datosAprendiz})
+  })
+  
+  })
 });
 
-app.put("/api/aprendices/:id", (req, res) => {
-    res.status(200).json({'Mensaje': 'Actualizar Aprendiz'})
-})
-
-app.delete("/api/aprendices", (req, res) => {
-    res.status(200).json({'Mensaje': 'Eliminado'})
-})
 
 
-app.listen(MIPUERTO, () => {
-    console.log(`Servidor ejecutándose en http://localhost:${MIPUERTO}`);
+app.put('/api/aprendices/:id', (req, res) => {
+res.status(200).json({Mensaje:"actualiza aprendices"})
+});
+
+app.delete('/api/aprendices', (req, res) => {
+res.status(200).json({Mensaje:"eliminado"})
+});
+
+app.listen(port, () => {
+console.log( `Servidor en funcionamiento en el puerto: http://localhost:${port}`);
 });
